825e7d778033ebe40157570853a20118da292568
amirali5370 <dr.amirali5370@gmail.com>
2025-09-16 13:24:19 +0330

    create user logout system

--- static/scripts/user/account.js
+++ static/scripts/user/account.js
@@ -17,3 +17,29 @@ function checking(event) {
     document.querySelector("form.password_change").submit();
     return true;
 }
+
+document.getElementById('out').addEventListener('click',(e) => {
+    Swal.fire({
+        title: 'آیا میخواهید از حساب کاربری خود، خارج شوید؟',
+        html: '',
+        icon: 'warning',
+        reverseButtons: true,
+        showCancelButton: true,
+        confirmButtonColor: '#F38524',
+        confirmButtonText: '<div style="direction:rtl;font-size:18px;font-family:Iran,Calibri;font-weight:bold;"><i style="margin-left: 5px;" class="fa fa-times"></i> تایید</div>',
+        cancelButtonText: '<div style="direction:rtl;font-size:18px;font-family:Iran,Calibri;font-weight:bold;border: 3 solid #ffd700;"><i style="margin-left: 5px;" class="fa fa-times"></i> انصراف</div>',
+        showLoaderOnConfirm:true,
+        allowOutsideClick:false,
+        showCloseButton: false,
+        focusConfirm: true,
+    }).then((result)=> {
+        if(result.value){
+            const xhr = new XMLHttpRequest();
+            var url = '/logout'
+            xhr.open('GET',url, true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send();
+            setInterval(function() {location.reload()}, 1000); 
+        };
+    });
+});
